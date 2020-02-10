ee3c952de4a7a972686cbf48496485439665090d
divukman <divukman@gmail.com>
2020-02-10 14:26:06 +0100

    Unit tests for Flux -> Examples.

--- src/test/java/com/dimitar/reactive/springreactive/fluxandmono/FluxAndMonoTest.java
+++ src/test/java/com/dimitar/reactive/springreactive/fluxandmono/FluxAndMonoTest.java
@@ -2,6 +2,7 @@ package com.dimitar.reactive.springreactive.fluxandmono;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
@@ -17,4 +18,65 @@ public class FluxAndMonoTest {
                 () -> System.out.println("Completed..."));
     }
 
+
+    @Test
+    public void testFluxElementsWithoutError() {
+        final Flux<String> strFlux =
+                Flux
+                        .just("Spring", "Spring Boot", "Reactive Spring")
+                        .log();
+
+        StepVerifier.create(strFlux)
+                .expectNext("Spring")
+                .expectNext("Spring Boot")
+                .expectNext("Reactive Spring")
+                .verifyComplete();
+    }
+
+
+    @Test
+    public void testFluxElementsWithError() {
+        final Flux<String> strFlux =
+                Flux
+                        .just("Spring", "Spring Boot", "Reactive Spring")
+                        .concatWith(Flux.error(new RuntimeException("test error")))
+                        .log();
+
+        StepVerifier.create(strFlux)
+                .expectNext("Spring")
+                .expectNext("Spring Boot")
+                .expectNext("Reactive Spring")
+               // .expectError(RuntimeException.class)
+                .expectErrorMessage("test error")
+                .verify();
+    }
+
+    @Test
+    public void testFluxElementsCountWithError() {
+        final Flux<String> strFlux =
+                Flux
+                    .just("Spring", "Spring Boot", "Reactive Spring")
+                    .concatWith(Flux.error(new RuntimeException("test error")))
+                    .log();
+
+        StepVerifier.create(strFlux)
+                .expectNextCount(3)
+                .expectErrorMessage("test error")
+                .verify();
+    }
+
+
+    @Test
+    public void testFluxElementsWithError_2() {
+        final Flux<String> strFlux =
+                Flux
+                        .just("Spring", "Spring Boot", "Reactive Spring")
+                        .concatWith(Flux.error(new RuntimeException("test error")))
+                        .log();
+
+        StepVerifier.create(strFlux)
+                .expectNext("Spring", "Spring Boot", "Reactive Spring")
+                .expectErrorMessage("test error")
+                .verify();
+    }
 }
